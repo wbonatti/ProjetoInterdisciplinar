3428563b1a73cb5e5a1a4ea1beba8969163f5b93
wellintonperazzoli <wellctba@gmail.com>
2014-09-11 23:54:38 -0300

    Finalizada a tela 'Geral'.

--- app/controllers/geralController.php
+++ app/controllers/geralController.php
@@ -8,12 +8,20 @@ Class geralController extends \BaseController
         $usuario = Usuario::where('id','=',$usuario['id'])->first();
         $aniversariantes = Pessoa::whereRaw('month(datanascimento) = month(now())')->get();
         $logs = UsuarioLog::where('usuario_id','=',$usuario->id)->orderByRaw('DATE(data) DESC')->limit(10)->get();
+        $numeroAlunos = Aluno::count();
+        $numeroFuncionario = Funcionario::count();
+        $numeroPessoa = Pessoa::count();
+        $numeroOutro = $numeroPessoa - $numeroAlunos - $numeroFuncionario;
         $mes = Carbon\Carbon::now()->formatLocalized('%B');
         $this->layout->content = View::make('geral.index')
         ->with([
             'aniversariantes' => $aniversariantes,
             'mes' => $mes,
-            'logs' => $logs
+            'logs' => $logs,
+            'numeroAluno' => $numeroAlunos,
+            'numeroFuncionario' => $numeroFuncionario,
+            'numeroOutro' => $numeroOutro,
+            'numeroPessoa' => $numeroPessoa
         ]);
     }
 }
--- app/views/geral/index.blade.php
+++ app/views/geral/index.blade.php
@@ -3,7 +3,35 @@
     <div class="main-inner">
         <div class="container">
             <div class="row">
+                <!-- /span6 -->
                 <div class="span6">
+                    <div class="widget widget-table action-table">
+                        <div class="widget-header"> <i class="icon-save"></i>
+                            <h3>Ultimos registros</h3>
+                        </div>
+                        <div class="widget-content">
+                            <table class="table table-striped table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th> Descrição </th>
+                                        <th> Data </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($logs as $log)
+                                        <?php $data = $log->getDateTime('data'); ?>
+                                        <tr>
+                                            <td> {{ $log->descricao }} </td>
+                                            <td> {{ $data->format('d/m/Y H:i:s') }} </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table> 
+                            <div class="widget-header text-right">
+                                <a href="javascript:;" class="btn btn-small btn-success adjust-footer-btn">Ver todos</a>
+                            </div>
+                        </div>
+                    </div>
                     <div class="widget widget-table action-table">
                         <div class="widget-header"> <i class="icon-gift"></i>
                             <h3>Aniversariantes no mês de {{ucfirst($mes)}}</h3>
@@ -39,149 +67,29 @@
                             </table>
                         </div> 
                     </div>
+                    
                 </div>
-                <!-- /span6 -->
+                <!-- /span6 --> 
                 <div class="span6">
-                    <div class="widget widget-table action-table">
-                        <div class="widget-header"> <i class="icon-save"></i>
-                            <h3>Ultimos registros</h3>
-                        </div>
-                        <div class="widget-content">
-                            <table class="table table-striped table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th> Descrição </th>
-                                        <th> Data </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($logs as $log)
-                                        <?php $data = $log->getDateTime('data'); ?>
-                                        <tr>
-                                            <td> {{ $log->descricao }} </td>
-                                            <td> {{ $data->format('d/m/Y H:i:s') }} </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table> 
-                            <div class="widget-header text-right">
-                                <a href="javascript:;" class="btn btn-small btn-success adjust-footer-btn">Ver todos</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="widget">
-                        <div class="widget-header"> <i class="icon-bookmark"></i>
-                            <h3>Important Shortcuts</h3>
-                        </div>
-                        <!-- /widget-header -->
-                        <div class="widget-content">
-                            <div class="shortcuts"> <a href="javascript:;" class="shortcut"><i class="shortcut-icon icon-list-alt"></i><span
-                                        class="shortcut-label">Apps</span> </a><a href="javascript:;" class="shortcut"><i
-                                        class="shortcut-icon icon-bookmark"></i><span class="shortcut-label">Bookmarks</span> </a><a href="javascript:;" class="shortcut"><i class="shortcut-icon icon-signal"></i> <span class="shortcut-label">Reports</span> </a><a href="javascript:;" class="shortcut"> <i class="shortcut-icon icon-comment"></i><span class="shortcut-label">Comments</span> </a><a href="javascript:;" class="shortcut"><i class="shortcut-icon icon-user"></i><span
-                                        class="shortcut-label">Users</span> </a><a href="javascript:;" class="shortcut"><i
-                                        class="shortcut-icon icon-file"></i><span class="shortcut-label">Notes</span> </a><a href="javascript:;" class="shortcut"><i class="shortcut-icon icon-picture"></i> <span class="shortcut-label">Photos</span> </a><a href="javascript:;" class="shortcut"> <i class="shortcut-icon icon-tag"></i><span class="shortcut-label">Tags</span> </a> </div>
-                            <!-- /shortcuts --> 
-                        </div>
-                        <!-- /widget-content --> 
-                    </div>
                     <!-- /widget -->
                     <div class="widget">
                         <div class="widget-header"> <i class="icon-signal"></i>
-                            <h3> Area Chart Example</h3>
+                            <h3>Estatísticas: Pessoas Cadastradas </h3>
                         </div>
                         <!-- /widget-header -->
                         <div class="widget-content">
-                            <canvas id="area-chart" class="chart-holder" height="250" width="538"> </canvas>
+                            <canvas id="area-chart" class="chart-holder" height="250" width="538" style="width: 100% !important; height: auto !important; margin: 0 auto;"> </canvas>
                             <!-- /area-chart --> 
-                        </div>
-                        <!-- /widget-content --> 
-                    </div>
-                    <!-- /widget -->
-                    <div class="widget widget-table action-table">
-                        <div class="widget-header"> <i class="icon-th-list"></i>
-                            <h3>A Table Example</h3>
-                        </div>
-                        <!-- /widget-header -->
-                        <div class="widget-content">
-                            <table class="table table-striped table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th> Free Resource </th>
-                                        <th> Download</th>
-                                        <th class="td-actions"> </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td> Fresh Web Development Resources </td>
-                                        <td> http://www.egrappler.com/ </td>
-                                        <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                                    </tr>
-                                    <tr>
-                                        <td> Fresh Web Development Resources </td>
-                                        <td> http://www.egrappler.com/ </td>
-                                        <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                                    </tr>
-                                    <tr>
-                                        <td> Fresh Web Development Resources </td>
-                                        <td> http://www.egrappler.com/ </td>
-                                        <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                                    </tr>
-                                    <tr>
-                                        <td> Fresh Web Development Resources </td>
-                                        <td> http://www.egrappler.com/ </td>
-                                        <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                                    </tr>
-                                    <tr>
-                                        <td> Fresh Web Development Resources </td>
-                                        <td> http://www.egrappler.com/ </td>
-                                        <td class="td-actions"><a href="javascript:;" class="btn btn-small btn-success"><i class="btn-icon-only icon-ok"> </i></a><a href="javascript:;" class="btn btn-danger btn-small"><i class="btn-icon-only icon-remove"> </i></a></td>
-                                    </tr>
-
-                                </tbody>
-                            </table>
-                        </div>
-                        <!-- /widget-content --> 
-                    </div>
-                    <!-- /widget --> 
-                    <div class="widget widget-nopad">
-                        <div class="widget-header"> <i class="icon-list-alt"></i>
-                            <h3> Recent News</h3>
-                        </div>
-                        <!-- /widget-header -->
-                        <div class="widget-content">
-                            <ul class="news-items">
-                                <li>
-
-                                    <div class="news-item-date"> <span class="news-item-day">29</span> <span class="news-item-month">Aug</span> </div>
-                                    <div class="news-item-detail"> <a href="http://www.egrappler.com/thursday-roundup-40/" class="news-item-title" target="_blank">Thursday Roundup # 40</a>
-                                        <p class="news-item-preview"> This is our web design and development news series where we share our favorite design/development related articles, resources, tutorials and awesome freebies. </p>
-                                    </div>
-
-                                </li>
-                                <li>
-
-                                    <div class="news-item-date"> <span class="news-item-day">15</span> <span class="news-item-month">Jun</span> </div>
-                                    <div class="news-item-detail"> <a href="http://www.egrappler.com/retina-ready-responsive-app-landing-page-website-template-app-landing/" class="news-item-title" target="_blank">Retina Ready Responsive App Landing Page Website Template – App Landing</a>
-                                        <p class="news-item-preview"> App Landing is a retina ready responsive app landing page website template perfect for software and application developers and small business owners looking to promote their iPhone, iPad, Android Apps and software products.</p>
-                                    </div>
-
-                                </li>
-                                <li>
-
-                                    <div class="news-item-date"> <span class="news-item-day">29</span> <span class="news-item-month">Oct</span> </div>
-                                    <div class="news-item-detail"> <a href="http://www.egrappler.com/open-source-jquery-php-ajax-contact-form-templates-with-captcha-formify/" class="news-item-title" target="_blank">Open Source jQuery PHP Ajax Contact Form Templates With Captcha: Formify</a>
-                                        <p class="news-item-preview"> Formify is a contribution to lessen the pain of creating contact forms. The collection contains six different forms that are commonly used. These open source contact forms can be customized as well to suit the need for your website/application.</p>
-                                    </div>
-
-                                </li>
-                            </ul>
+                            <hr>
+                            <p class="text-danger"><a class="btn btn-danger"></a> <b>Alunos:</b> {{$numeroAluno}} ({{$numeroPessoa / $numeroAluno * 10}}%)</p>
+                            <p class="text-success"><a class="btn btn-success"></a> <b>Funcionários:</b> {{$numeroFuncionario}} ({{$numeroPessoa / $numeroFuncionario * 10}}%)</p>
+                            <p class="text-info"><a class="btn btn-info"></a> <b>Outros:</b> {{$numeroOutro}} ({{$numeroPessoa / $numeroOutro * 10}}%) </p>
+                            <p><b>Total:</b> {{$numeroPessoa}} pessoas cadastradas.</p>
                         </div>
                         <!-- /widget-content --> 
                     </div>
                     <!-- /widget -->
                 </div>
-                <!-- /span6 --> 
             </div>
             <!-- /row --> 
         </div>
@@ -191,47 +99,22 @@
 </div>
 
 <script>
+    var graficoData = [
+				{
+				    value: {{$numeroPessoa / $numeroAluno * 10}},
+				    color: "#db3325"
+				},
+				{
+				    value: {{$numeroPessoa / $numeroFuncionario * 10}},
+				    color: "#7eb216"
+				},
+				{
+				    value: {{$numeroPessoa / $numeroOutro * 10}},
+				    color: "#4eb2d5"
+				}
+			];
 
-    var lineChartData = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                fillColor: "rgba(220,220,220,0.5)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                data: [65, 59, 90, 81, 56, 55, 40]
-            },
-            {
-                fillColor: "rgba(151,187,205,0.5)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                data: [28, 48, 40, 19, 96, 27, 100]
-            }
-        ]
-
-    }
-
-    var myLine = new Chart(document.getElementById("area-chart").getContext("2d")).Line(lineChartData);
-
-
-    var barChartData = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                fillColor: "rgba(220,220,220,0.5)",
-                strokeColor: "rgba(220,220,220,1)",
-                data: [65, 59, 90, 81, 56, 55, 40]
-            },
-            {
-                fillColor: "rgba(151,187,205,0.5)",
-                strokeColor: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 96, 27, 100]
-            }
-        ]
-
-    }
+    var EstatisticaPessoa = new Chart(document.getElementById("area-chart").getContext("2d")).Doughnut(graficoData);
 
     $(document).ready(function() {
         var date = new Date();
